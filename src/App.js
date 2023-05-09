6561c5524d896ec0dffde952c167efa5e9099062
Dilay Kirisci <isik.kirisci@gmail.com>
2023-05-09 15:26:28 +0300

    token added

--- src/App.js
+++ src/App.js
@@ -1,41 +1,44 @@
 import "./App.css";
 import { NavLink, Switch, Route } from "react-router-dom";
 import SignUp from "./forms/SignUp";
+import { TokenProvider } from "./context/TokenProvider";
 
 function App() {
-  return (
-    <div className="px-10 py-6 max-w-6xl mx-auto">
-      <header>
-        <nav className="flex gap-2 text-2xl font-serif">
-          <NavLink activeClassName="text-purple-600" exact to="/">
-            Anasayfa
-          </NavLink>
-          <NavLink activeClassName="text-purple-600" to="/giris-yap">
-            Giriş yap
-          </NavLink>
-          <NavLink activeClassName="text-purple-600" to="/kayit-ol">
-            Kayıt ol
-          </NavLink>
-          <NavLink activeClassName="text-purple-600" to="/profilim">
-            Profilim
-          </NavLink>
-        </nav>
-      </header>
-      <main className="mt-6">
-        <Switch>
-          <Route exact path="/">
-            /
-          </Route>
-          <Route path="/giris-yap">giris-yap</Route>
-          <Route path="/kayit-ol">
-            <SignUp />
-          </Route>
-          {/* <PrivateRoute path="/profilim">profilim</PrivateRoute> SignIn öncesi bu şekilde olması gerekir diye düşünüyorum*/}
-          <Route path="/profilim">profilim</Route>
-        </Switch>
-      </main>
-    </div>
-  );
+	return (
+		<TokenProvider>
+			<div className="px-10 py-6 max-w-6xl mx-auto">
+				<header>
+					<nav className="flex gap-2 text-2xl font-serif">
+						<NavLink activeClassName="text-purple-600" exact to="/">
+							Anasayfa
+						</NavLink>
+						<NavLink activeClassName="text-purple-600" to="/giris-yap">
+							Giriş yap
+						</NavLink>
+						<NavLink activeClassName="text-purple-600" to="/kayit-ol">
+							Kayıt ol
+						</NavLink>
+						<NavLink activeClassName="text-purple-600" to="/profilim">
+							Profilim
+						</NavLink>
+					</nav>
+				</header>
+				<main className="mt-6">
+					<Switch>
+						<Route exact path="/">
+							/
+						</Route>
+						<Route path="/giris-yap">giris-yap</Route>
+						<Route path="/kayit-ol">
+							<SignUp />
+						</Route>
+						{/* <PrivateRoute path="/profilim">profilim</PrivateRoute> SignIn öncesi bu şekilde olması gerekir diye düşünüyorum*/}
+						<Route path="/profilim">profilim</Route>
+					</Switch>
+				</main>
+			</div>
+		</TokenProvider>
+	);
 }
 
 export default App;
