a2771bce662378e77e400de885c5aed38aeb32c4
Dilay Kirisci <isik.kirisci@gmail.com>
2023-05-11 09:50:36 +0300

    update cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,29 +1,25 @@
 import Card from "./Card";
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import axios from "axios";
 import { TokenContext } from "../context/TokenProvider";
 export default function Cards() {
-    const { token } = useContext(TokenContext);
-    const [data, setData] = useState([]);
-    axios
-        .get("https://wit-courses-api2.onrender.com/entries", {
-        headers: {
-            Authorization: token,
-        },
-    })
-        .then((response) => setData(...data, response.data))
-        .catch((error) => console.log(error));
-    console.log(data);
+	const { token } = useContext(TokenContext);
+	const [data, setData] = useState([]);
+	useEffect(() => {
+		axios
+			.get("https://wit-courses-api2.onrender.com/entries", {
+				headers: {
+					Authorization: token,
+				},
+			})
+			.then((response) => setData(...data, response.data))
+			.catch((error) => console.log(error));
+	}, []);
+	console.log(data);
 
-    const renderedCards = data.map((tweet) => {
-        return <Card key={tweet.id} tweets={tweet} />;
-    });
+	const renderedCards = data.map((tweet) => {
+		return <Card key={tweet.id} tweets={tweet} />;
+	});
 
-   
-
-    return (
-        <div className="cards">
-            {renderedCards}
-        </div>
-    )
+	return <div className="cards">{renderedCards}</div>;
 }
